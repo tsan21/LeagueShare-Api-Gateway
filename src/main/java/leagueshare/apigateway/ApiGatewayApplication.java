3b21f0deb0060ef349ce181b52102029874ae1aa
tsan <tsanyip@hotmail.com>
2021-11-19 14:53:27 +0100

    init api gateway

---
+++ src/main/java/leagueshare/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package leagueshare.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/demo")
+//						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://demo-service:8090"))
+				.build();
+	}
+
+}
